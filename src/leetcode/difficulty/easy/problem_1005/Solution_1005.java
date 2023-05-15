a936aa94a347e8ba1509225dcaaa7e38f020308a
hemgom <reborn.april23@gmail.com>
2023-05-16 00:07:58 +0900

    problem solved (25.05.15)

--- src/leetcode/difficulty/easy/problem_1005/Solution_1005.java
+++ src/leetcode/difficulty/easy/problem_1005/Solution_1005.java
@@ -1,12 +1,28 @@
 package leetcode.difficulty.easy.problem_1005;
 
 public class Solution_1005 {
-    public static void main(String[] args) {
-        int a = 1;
-        int min;
+    public int largestSumAfterKNegations(int[] nums, int k) {
+        int minNum = nums[0];
+        int minIndex = 0;
+        int sum = 0;
 
-        min = -a;
+        while (k != 0) {
+            for (int i = 1; i < nums.length; i++) {
+                if (minNum > nums[i]) {
+                    minNum = nums[i];
+                    minIndex = i;
+                }
+            }
+            nums[minIndex] = -minNum;
+            k -= 1;
+            minNum = nums[0];
+            minIndex = 0;
+        }
 
-        System.out.println(min);
+        for (int j = 0; j < nums.length; j++) {
+            sum += nums[j];
+        }
+
+        return sum;
     }
 }
