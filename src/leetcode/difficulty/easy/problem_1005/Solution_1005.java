30ef15c595e852033375dfd2b6409ad7b2b227df
hemgom <reborn.april23@gmail.com>
2023-05-15 22:53:30 +0900

    problem solved (25.05.15)

---
+++ src/leetcode/difficulty/easy/problem_1005/Solution_1005.java
@@ -0,0 +1,12 @@
+package leetcode.difficulty.easy.problem_1005;
+
+public class Solution_1005 {
+    public static void main(String[] args) {
+        int a = 1;
+        int min;
+
+        min = -a;
+
+        System.out.println(min);
+    }
+}
