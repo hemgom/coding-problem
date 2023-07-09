f5f26ea98d49059593b4c24569a8b5551c898183
hemgom <reborn.april23@gmail.com>
2023-07-09 20:58:55 +0900

    problem solved (23.07.09)

---
+++ src/leetcode/difficulty/easy/problem_836/Solution_836.java
@@ -0,0 +1,10 @@
+package leetcode.difficulty.easy.problem_836;
+
+public class Solution_836 {
+    public boolean isRectangleOverlap(int[] rec1, int[] rec2) {
+        return rec1[0] < rec2[2] &&
+                rec1[2] > rec2[0] &&
+                rec1[1] < rec2[3] &&
+                rec1[3] > rec2[1];
+    }
+}
