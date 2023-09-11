d09ec450e80cc51f744eda272865024092f6def5
hemgom <reborn.april23@gmail.com>
2023-09-11 17:45:31 +0900

    problem solved (23.09.11)

---
+++ src/leetcode/difficulty/easy/problem_326/Solution_326.java
@@ -0,0 +1,17 @@
+package leetcode.difficulty.easy.problem_326;
+
+public class Solution_326 {
+    public boolean isPowerOfThree(int n) {
+
+        if (n == 0) return false;
+
+        while (n != 1) {
+
+            if (n % 3 != 0) return false;
+
+            n /= 3;
+        }
+
+        return true;
+    }
+}
